1df69033e5d67cad4afe355680f1dd9c3ba01ab7
Jacob Stone <jumboshrimp9554@gmail.com>
2025-02-26 22:12:57 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+Result checkChar(char correctChar, char guessChar, int idx);
+
+int main();
+
+int main(void) {
+    printf("Hello World!\n");
+
+    return 1;
+}
